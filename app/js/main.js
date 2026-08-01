0fc7fe309ee0e8d3ef99ca14663e23f0624d0603
drozdtatsiana <drozdtatsiana0@gmail.com>
2026-08-01 14:04:35 +0300

    added section look

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.look__slider', {
+
+  loop: true,
+  slidesPerView: 3,
+        spaceBetween: 40,
+  navigation: {
+    nextEl: '.look__button-next',
+    prevEl: '.look__button-prev',
+  },
+
+});
+
+
+
