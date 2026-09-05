001cf101064dac2cb84c3153fcbed6336e68020e
drozdtatsiana <drozdtatsiana0@gmail.com>
2026-09-05 12:57:29 +0300

    added page checkout and page catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,7 @@
-const swiper = new Swiper('.look__slider', {
-
-  loop: true,
-  slidesPerView: 3,
-        spaceBetween: 40,
-  navigation: {
-    nextEl: '.look__button-next',
-    prevEl: '.look__button-prev',
-  },
-
-});
-
-
 const reviewsSwiper = new Swiper('.reviews__slider', {
  
   loop: true,
-    slidesPerView: 9,
+    slidesPerView: 8.5,
         spaceBetween: 16,
 
   
@@ -29,4 +16,67 @@ const reviewsSwiper = new Swiper('.reviews__slider', {
   },
 });
 
+const modeContainer = document.querySelector('.view-mode__container');
+const modeBtnGrid = document.querySelector('.view-mode__btn-grid');
+const modeBtnLine = document.querySelector('.view-mode__btn-line');
+
+modeBtnGrid .addEventListener('click', () => {
+modeContainer.classList.add('view-mode__container--grid');
+modeContainer.classList.remove('view-mode__container--line');
+})
+
+modeBtnLine .addEventListener('click', () => {
+modeContainer.classList.add('view-mode__container--line');
+modeContainer.classList.remove('view-mode__container--grid');
+})
+
+
+const swiper = new Swiper('.look__slider', {
+
+  loop: true,
+  slidesPerView: 3,
+        spaceBetween: 40,
+  navigation: {
+    nextEl: '.look__button-next',
+    prevEl: '.look__button-prev',
+  },
+
+});
+
+
+
+
+const rangeSlider = document.querySelector('.range__slider');
+const rangeMin = document.querySelector('.range__min');
+const rangeMax = document.querySelector('.range__max');
+var slider = document.getElementById('slider');
+
+noUiSlider.create(rangeSlider, {
+    start: [300, 3000],
+    step: 100,
+    range: {
+        'min': 300,
+        'max': 3000
+    },
+    format: {
+    to: value => Math.round(value),
+    from: value => Number(value)
+    }
+});
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+    if (handle === 0) {
+        rangeMin.value = values[0];
+    } else {
+        rangeMax.value  = values[1];
+    }
+});
+
+rangeMin.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([rangeMin.value, null]);
+});
+
+rangeMax.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([null,  rangeMax.value]);
+});
 
